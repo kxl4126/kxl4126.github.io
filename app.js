580515f0f41bf5df17b9bdaae1323e16c9b16c1f
kevinli <kevkev00@gmail.com>
2020-05-21 00:26:40 -0500

    exchange code for auth token

--- app.js
+++ app.js
@@ -4,24 +4,21 @@
 const queryString = window.location.search;
 console.log(queryString);
 const urlParams = new URLSearchParams(queryString);
-const code = urlParams.get('code');
-if (code) { //authenticate user and pass to backend
+const gen_code = urlParams.get('code');
+if (gen_code) { //authenticate user and pass to backend
 
-    console.log("Code: " + code);
+    console.log("Code: " + gen_code);
 
-    $.ajax({
-        url: 'https://www.linkedin.com/oauth/v2/accessToken',
-        type: 'post',
-        data: {
-            access_token: code
-        },
-        headers: {
-            "Content-Type": 'pplication/x-www-form-urlencoded'
-        },
-        dataType: 'json',
-        success: function (data) {
-            console.info(data);
-        }
+    $.post("https://www.linkedin.com/oauth/v2/accessToken",
+      {
+        grant_type: 'authorization_code',
+        code: gen_code,
+        redirect_uri: 'https://kxl4126.github.io',
+        client_id: '860zh5czatlpnt',
+        client_secret: 'KoYtnFvlSAHaSNWO',
+      },
+      function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
     });
 
 }
